a30c1bd30bacc6c0ecd6624d880ec50e2ee820c4
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2024-04-29 20:08:00 +0200

    Slint UI Test

--- prolog/riscv64.pl
+++ prolog/riscv64.pl
@@ -3,6 +3,7 @@
 :- use_module(library(dcgs)).
 :- use_module(library(format)).
 :- use_module(library(lists)).
+:- use_module(library(lambda)).
 
 % reg(Name, Code, Description)
 reg(x0, 0, "zero").
@@ -13,6 +14,11 @@ reg(x2, 2, "zero").
 % Coding can be
 % - i(Opcode, Funct3)
 instr(addi, i(19, 0), "ADDI adds the sign-extended 12-bit immediate to register rs1. Arithmetic overflow is ignored and the result is simply the low XLEN bits of the result").
+instr(slti, i(19, 2), "SLTI (set less than immediate) places the value 1 in register rd if register rs1 is less than the sign-extended immediate when both are treated as signed numbers, else 0 is written to rd. SLTIU is similar but compares the values as unsigned numbers (i.e., the immediate is first sign-extended to XLEN bits then treated as an unsigned number). Note, SLTIU rd, rs1, 1 sets rd to 1 if rs1 equals zero, otherwise sets rd to 0 (assembler pseudoinstruction SEQZ rd, rs).").
+instr(sltiu, i(19, 3), Doc) :- instr(slti, _, Doc).
+instr(andi, i(19, 7), "ANDI, ORI, XORI are logical operations that perform bitwise AND, OR, and XOR on register rs1 and the sign-extended 12-bit immediate and place the result in rd. Note, XORI rd, rs1, -1 performs a bitwise logical inversion of register rs1 (assembler pseudoinstruction NOT rd, rs).").
+instr(ori, i(19, 6), Doc) :- instr(andi, _, Doc).
+instr(xori, i(19, 4), Doc) :- instr(andi, _, Doc).
 
 instr_doc_(InstrName) -->
     { instr(InstrName, i(Opcode, Funct3), Doc), format_binary(7, Opcode, OpcodeBin), format_binary(3, Funct3, Funct3Bin) },
@@ -27,14 +33,14 @@ instr_doc(InstrName) :-
     phrase(instr_doc_(InstrName), Text),
     format("~s", [Text]).
 
-format_binary(0, _, "").
-format_binary(N, Code, Bin) :-
-    B #= Code mod 2,
-    C0 #= Code div 2,
-    N0 #= N - 1,
-    format_binary(N0, C0, B0),
-    number_chars(B, BStr),
-    append(B0, BStr, Bin).
+all_instr(I) :-
+    findall(NameStr, (instr(Name, _, _), atom_chars(Name, NameStr)), I0), sort(I0, I).
+
+format_binary(L, N, S) :-
+    length(S, L),    
+    append(S0, S1, S),
+    maplist(\X^(X='0'), S0),
+    phrase(format_("~2r", [N]), S1).
 
 % Instr can be instr(addi, [reg(x1), reg(x2), imm(100)])
 % TODO: Add reverse
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,93 @@
+use scryer_prolog::machine::*;
+use scryer_prolog::machine::parsed_results::*;
+use slint::{ModelRc, StandardListViewItem, VecModel};
+use std::rc::Rc;
+
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    let prolog_code = include_str!("../prolog/riscv64.pl");
+    let mut machine = Machine::new_lib();
+    machine.load_module_string("riscv64", prolog_code.to_string());
+    let results = machine.run_query("all_instr(X).".to_string());
+    let mut instructions: Vec<StandardListViewItem> = Vec::new();
+    match results {
+	Ok(query_res) => match query_res {
+	    QueryResolution::Matches(matches) => {
+		let bindings = matches[0].bindings.clone();
+		if let Some(value) = bindings.get("X") {
+		    match value {
+			Value::List(list) => {
+			    for x in list {
+				match x {
+				    Value::String(string) => {
+					instructions.push(StandardListViewItem::from(string.as_str()));
+				    },
+				    _ => unimplemented!()
+				}
+			    }
+			},
+			_ => unimplemented!()
+		    }
+		}
+	    }
+	    _ => unimplemented!()
+	},
+	_ => unimplemented!()
+    };
+    
+    
+    let window = MainWindow::new().unwrap();
+    let model = Rc::new(VecModel::from(instructions));
+    window.set_instructions(ModelRc::from(model.clone()));
+
+    window.on_instr_doc(move |instr| {
+	let mut output = String::new();
+	match machine.run_query(format!("phrase(instr_doc_({}), X).", instr.clone())) {
+	    Ok(query_res) => match query_res {
+		QueryResolution::Matches(matches) => {
+		    let bindings = matches[0].bindings.clone();
+		    if let Some(value) = bindings.get("X") {
+			match value {
+			    Value::String(string) => {
+				output = string.replace("\\n", "\n");
+			    }
+			    _ => ()
+			}
+		    }
+		}
+		_ => ()
+	    }
+	    _ => ()
+	};
+	output.into()
+    });
+
+    window.run().unwrap();
 }
 
 slint::slint! {
+    import { HorizontalBox, StandardListView, TabWidget, TextEdit } from "std-widgets.slint";
     export component MainWindow inherits Window {
-	Text {
-	    text: "Hello World";
-	    color: green;
+	in-out property<[StandardListViewItem]> instructions: [{text: "Hola"}, {text: "Adios"}];
+	callback instr_doc(string) -> string;
+	TabWidget {
+	    Tab {
+		title: "RISC-V Reference";
+		property<string> referenceText: "";
+		HorizontalLayout {
+		    StandardListView {
+			width: 150px;
+			model: instructions;
+			current-item-changed(i) => {
+			    referenceText = instr_doc(instructions[i].text);
+			}
+		    }
+		    TextEdit {
+			horizontal-stretch: 1;
+			text: referenceText;
+			read-only: true;
+		    }
+		}
+	    }
 	}
     }
 }
